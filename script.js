f81774869c0249c17f5d0a8f545a5c010f52efd6
Mohit Kumar <109014039+Mohitkumar521@users.noreply.github.com>
2023-11-22 13:10:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+for (let i = 1; i <= 20; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    alert("FizzBuzz");
+  } else if (i % 3 === 0) {
+    alert("Fizz");
+  } else if (i % 5 === 0) {
+    alert("Buzz");
+  } else {
+    alert(i);
+  }
+}
+for (let i = 1; i <= 20; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  }
+}
